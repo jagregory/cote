2be85e6e1c880a469c94f9077a07d7b2375c0bb5
James Gregory <james@jagregory.com>
2013-10-04 11:32:22 +1000

    Can load input template from a named file

--- main.go
+++ main.go
@@ -3,20 +3,74 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path"
 )
 
 var templateName = flag.String("name", "", "Name of the template, this will be the function name")
+var inputFile = flag.String("input", "", "Template path e.g. templates/example.cote")
 
 func main() {
 	flag.Parse()
 
-	if templateName == nil || *templateName == "" {
-		fmt.Fprintln(os.Stderr, "Template name not specified. Use -name flag.")
-		os.Exit(1)
-	}
+	var out io.Writer
 
-	if err := Convert(*templateName, os.Stdin, os.Stdout); err != nil {
+	in, name := readInput()
+	// TODO: optional output file flag
+	out = os.Stdout
+
+	if err := Convert(name, in, out); err != nil {
 		panic(err)
 	}
+}
+
+// Reads the input file from the -input flag or Stdin. Exits if
+// no template name given when using stdin, or no input path given.
+func readInput() (f io.Reader, name string) {
+	if isUsingStdin() {
+		if *templateName == "" {
+			fmt.Fprintln(os.Stderr, "Template name not specified. Use -name flag.")
+			os.Exit(1)
+		}
+
+		f = os.Stdin
+		name = *templateName
+	} else {
+		if *inputFile == "" {
+			fmt.Fprintln(os.Stderr, "Input path not specified. Use -input flag or Stdin")
+			os.Exit(1)
+		}
+
+		var err error
+		f, err = os.Open(*inputFile)
+		if err != nil {
+			panic(err)
+		}
+
+		name = *templateName
+		if name == "" {
+			name = templateNameFromFilePath(*inputFile)
+		}
+	}
+
+	return
+}
+
+// Extract a sensible template name from the file path
+func templateNameFromFilePath(p string) (name string) {
+	name = path.Base(p)
+	if ext := path.Ext(name); ext != "" {
+		name = name[:len(name)-len(ext)]
+	}
+	return
+}
+
+func isUsingStdin() bool {
+	s, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	return s.Size() > 0
 }
